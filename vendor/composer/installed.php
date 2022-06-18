0acce81213e3724f602d3dc6634bbc44f94f5868
Máximo Perez Villalba <maximo.villalba@gmail.com>
2022-06-18 00:59:10 -0300

    v2.1.0 fix HTTPRequestsRoutes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'maximo-perez-villalba/framework-http',
+        'pretty_version' => '2.0.0',
+        'version' => '2.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'maximo-perez-villalba/framework-environment' => array(
+            'pretty_version' => '1.1.2',
+            'version' => '1.1.2.0',
+            'reference' => '6b31a07ee3088851cab936f567566f80192cc62d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../maximo-perez-villalba/framework-environment',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'maximo-perez-villalba/framework-http' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
